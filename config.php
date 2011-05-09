e360b9d3458b1c93801b9de71c731f05279d1307
Ionuț G. Stan <ionut.g.stan@gmail.com>
2011-05-09 19:48:44 +0300

    Support PHP < 5.3

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BUILD_DIR',      __DIR__ . '/build');
+define('BUILD_DIR',      dirname(__FILE__) . '/build');
 define('EBOOK_TITLE',    'Learn You Some Erlang for Great Good!');
 define('EBOOK_AUTHOR',   'Frederic Trottier-Hebert');
 define('EBOOK_LANGUAGE', 'en-us');
